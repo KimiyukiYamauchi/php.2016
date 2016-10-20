e46c4ad33a464186e1f4f575dd99d0935e14d000
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-20 19:33:46 +0900

    ２つの文字列が等しいかの判定方法

--- text/Chapter3/example-3-14.php
+++ text/Chapter3/example-3-14.php
@@ -1,3 +1,13 @@
+<?php
+
+$first_name = 'test';
+$last_name = 'test';
+
+// strcasecmp関数は引数の2つの文字列が等しい場合は
+// 0を返す、phpは0をfalseと見るので、
+// それの否定(!)はtrueになる
 if (! strcasecmp($first_name,$last_name)) {
-    print '$first_name and $last_name are equal.';
+    print '２つの文字列は等しいです。';
+}else{
+	echo '２つの文字列は等しくありません。';
 }
