b5399d366f5c27dc3519c809efa95db91e614430
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-20 20:16:02 +0900

    配列の動作確認(p57まで)

--- text/Chapter4/example-4-02.php
+++ text/Chapter4/example-4-02.php
@@ -1,11 +1,23 @@
+<?php
+
+// 配列名の命名規則(変数の命名規則と同一)
+// 1文字目がアルファベットまたはアンダスコア(_)
+// 2文字目以降はアルファベット、アンダスコア(_)または数字
+
 $vegetables = array('corn' => 'yellow',
                     'beet' => 'red',
                     'carrot' => 'orange');
+
+var_dump($vegetables);
 
 $dinner = array(0 => 'Sweet Corn and Asparagus',
                 1 => 'Lemon Chicken',
                 2 => 'Braised Bamboo Fungus');
 
+var_dump($dinner);
+
 $computers = array('trs-80' => 'Radio Shack',
                    2600 => 'Atari',
                    'Adam' => 'Coleco');
+
+var_dump($computers);
