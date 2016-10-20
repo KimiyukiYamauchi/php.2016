55121ccf0f724cb769a6daed1fa6cb68daceeaa6
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-20 19:19:31 +0900

    2つの少数点数が等しいかどうかの判定

--- text/Chapter3/example-3-09.php
+++ text/Chapter3/example-3-09.php
@@ -1,6 +1,18 @@
+<?php
 
+$price_1 = 1.01;
+$price_2 = 1.01;
+
+// こんな比較はやらないほうがいい
+//if($price_1 == $price_2)
+// 2つの少数点数の差が0.00001よりも小さい時は等しいとする
 if(abs($price_1 - $price_2) < 0.00001) {
     print '$price_1 and $price_2 are equal.';
 } else {
     print '$price_1 and $price_2 are not equal.';
 }
+
+
+
+
+
