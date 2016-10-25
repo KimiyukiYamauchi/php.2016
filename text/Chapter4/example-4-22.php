a9b69dcaa787886725765c21173c160295a24e28
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-25 19:25:04 +0900

    explode関数の使い方、動作確認

--- text/Chapter4/example-4-22.php
+++ text/Chapter4/example-4-22.php
@@ -1,3 +1,9 @@
-$fish = 'Bass, Carp, Pike, Flounder';
-$fish_list = explode(', ', $fish);
+<?php
+
+//$fish = 'Bass, Carp, Pike, Flounder';
+$fish = 'Bass:Carp:Pike:Flounder:test:testtest:testtesttest';
+//$fish_list = explode(', ', $fish);
+$fish_list = explode(':', $fish);
 print "The second fish is $fish_list[1]";
+
+var_dump($fish_list);
