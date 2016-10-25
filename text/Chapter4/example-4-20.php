7b4c60454bdc6159547c087e178d7d08d1e68051
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-25 19:24:27 +0900

    implode関数の使い方、動作確認

--- text/Chapter4/example-4-20.php
+++ text/Chapter4/example-4-20.php
@@ -1,3 +1,6 @@
+<?php
+
 $dimsum = array('Chicken Bun','Stuffed Duck Web','Turnip Cake');
-$menu = implode(', ', $dimsum);
+//$menu = implode(', ', $dimsum);
+$menu = '<ul><li>' . implode('</li><li>', $dimsum) . '</li></ul>';
 print $menu;
