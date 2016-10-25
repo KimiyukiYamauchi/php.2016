df2a9fae470ebb1c35718084b2998db7e5935449
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-25 21:42:35 +0900

    5.1 関数の宣言と呼び出しの動作確認

--- text/Chapter5/example-5-03.php
+++ text/Chapter5/example-5-03.php
@@ -1,7 +1,11 @@
+<?php
+
 function page_header() {
     print '<html><head><title>Welcome to my site</title></head>';
     print '<body bgcolor="#ffffff">';
 }
+
+$user = 'やまうち';
 
 page_header();
 print "Welcome, $user";
