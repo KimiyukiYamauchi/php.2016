e5ec3767d74c2b139a1fcef67ca65171a0186cef
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-25 21:58:01 +0900

    5.2 関数へ引数を渡す P80まで

--- text/Chapter5/example-5-03.php
+++ text/Chapter5/example-5-03.php
@@ -5,9 +5,28 @@ function page_header() {
     print '<body bgcolor="#ffffff">';
 }
 
+function page_header2($color) {
+    print '<html><head><title>Welcome to my site</title></head>';
+    print '<body bgcolor="#' . $color . '">';
+
+}
+
+function page_header3($color = 'cc3399') {
+    print '<html><head><title>Welcome to my site</title></head>';
+    print '<body bgcolor="#' . $color . '">';
+}
+
+function page_header4($color, $title) {
+    print '<html><head><title>Welcome to ' . $title . '</title></head>';
+    print '<body bgcolor="#' . $color . '">';
+}
+
 $user = 'やまうち';
 
-page_header();
+//page_header();
+//page_header2();
+//page_header3();
+page_header4('66cc66', 'my homepage');
 print "Welcome, $user";
 page_footer();
 
