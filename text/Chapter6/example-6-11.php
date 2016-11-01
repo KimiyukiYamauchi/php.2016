fd18c8c03738d0c00ba230ed797ddad6bb90f0d2
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-01 20:07:39 +0900

    6.3.2 数値要素または文字列要素 例6-11の動作確認

--- text/Chapter6/example-6-11.php
+++ text/Chapter6/example-6-11.php
@@ -1,3 +1,12 @@
-if ($_POST['age'] != strval(intval($_POST['age'])) {
+<?php
+
+$errors = array();
+$_POST['age'] = '4A';
+
+if ($_POST['age'] != strval(intval($_POST['age']))) {
     $errors[] = 'Please enter a valid age.';
+}else{
+	$errors[] = "OK!(本来はメッセージは追加されない)";
 }
+
+var_dump($errors);
