f92ce86eb2e9e01a416e2a8a2ace35438595f37e
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-01 20:30:32 +0900

    例6-13 trim()とstrlen()の結合、例6-14 の値を変更する

--- text/Chapter6/example-6-13.php
+++ text/Chapter6/example-6-13.php
@@ -1,3 +1,14 @@
-if (strlen(trim($_POST['name'])) == 0) {
+<?php
+
+$errors = array();
+$_POST['name'] = '    a     ';
+
+$_POST['name'] = trim($_POST['name']);
+
+if (strlen($_POST['name']) == 0) {
     $errors[] = "Your name is required.";
+}else{
+	$errors[] = "OK!(本来はメッセージは追加されない)";
 }
+
+var_dump($errors);
