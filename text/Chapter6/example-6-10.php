4418eff995371f84d375b024b0ba6500d1c6cb92
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-01 20:06:52 +0900

    6.3.1 必須の要素 例6-10の動作確認

--- text/Chapter6/example-6-10.php
+++ text/Chapter6/example-6-10.php
@@ -1,3 +1,12 @@
+<?php
+
+$errors = array();
+$_POST['email'] = 'test@test';
+
 if (strlen($_POST['email']) == 0) {
    $errors[] = "You must enter an e-mail address.";
+}else{
+	$errors[] = "OK!(本来はメッセージは追加されない)";
 }
+
+var_dump($errors);
