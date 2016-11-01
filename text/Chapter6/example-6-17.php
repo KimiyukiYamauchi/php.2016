1c872ae893c96354ce1a58e5d6ab8b00988fef55
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-01 21:58:54 +0900

    例6-17 Eメールアドレスの構文をチェクしますの動作確認

--- text/Chapter6/example-6-17.php
+++ text/Chapter6/example-6-17.php
@@ -1,4 +1,13 @@
+<?php
+
+$errors = array();
+$_POST['email'] = 'aaaaa@te-st.ab';
+
 if (! preg_match('/^[^@\s]+@([-a-z0-9]+\.)+[a-z]{2,}$/i', 
                  $_POST['email'])) {
     $errors[] = 'Please enter a valid e-mail address';
+}else{
+	$errors[] = "OK!(本来はメッセージは追加されない)";
 }
+
+var_dump($errors);
