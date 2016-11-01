40567beea80a6f39921c8fcabf6aa0a252acd0b1
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-01 20:16:59 +0900

    例6-12 浮動小数点数のチェック

--- text/Chapter6/example-6-12.php
+++ text/Chapter6/example-6-12.php
@@ -1,3 +1,12 @@
+<?php
+
+$errors = array();
+$_POST['price'] = '59.2a';
+
 if ($_POST['price'] != strval(floatval($_POST['price']))) {
     $errors[] = 'Please enter a valid price.';
+}else{
+	$errors[] = "OK!(本来はメッセージは追加されない)";
 }
+
+var_dump($errors);
