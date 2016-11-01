92235bfbcea0546af5c44a44a4f5bd04495c2426
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-01 20:55:21 +0900

    例6-15 数の範囲をチェックする

--- text/Chapter6/example-6-15.php
+++ text/Chapter6/example-6-15.php
@@ -1,5 +1,14 @@
+<?php
+
+$errors = array();
+$_POST['age'] = '17';
+
 if ($_POST['age'] != strval(intval($_POST['age']))) {
     $errors[] = "Your age must be a number.";
 } elseif (($_POST['age'] < 18) || ($_POST['age'] > 65)) {
     $errors[] = "Your age must be at least 18 and no more than 65.";
+}else{
+	$errors[] = "OK!(本来はメッセージは追加されない)";
 }
+
+var_dump($errors);
