5926c8a947968899448f7882d255e9d4e5613700
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-09 20:33:38 +0900

    A.6.3 配列処理の動作確認

--- text/AppendixB/example-B-09.php
+++ text/AppendixB/example-B-09.php
@@ -1,5 +1,9 @@
+<?php
+
 $sea_creatures = "cucumber;jellyfish, conger eel,shrimp, crab roe; bluefish";
 // Break apart the string on a comma or semicolon
 // followed by an optional space
-$creature_list = preg_split('/[,;] ?/',$sea_creatures);
+$creature_list = preg_split('/[,;] ?/',$sea_creatures, 3);
 print "Would you like some $creature_list[2]?";
+
+var_dump($creature_list);
--- text/AppendixB/example-B-12.php
+++ text/AppendixB/example-B-12.php
@@ -1,3 +1,5 @@
+<?php
+
 $text=<<<TEXT
 "It's time to ring again," said Tom rebelliously.
 "I agree! I'll help you," said Jerry resoundingly.
@@ -8,6 +10,8 @@ $words = preg_split('/[",.!\s]/', $text, -1, PREG_SPLIT_NO_EMPTY);
 // Find words that contain double letters
 $double_letter_words = preg_grep('/([a-z])\\1/i',$words);
 
+var_dump($words);
+
 foreach ($double_letter_words as $word) {
-    print "$word\n";
+    print "$word<br>\n";
 }
