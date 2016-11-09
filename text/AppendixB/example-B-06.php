55aa319d75c0d3e2dc7ccd8fd7e48c8e839d0b7d
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-09 19:19:22 +0900

    リスト A.6 逆引きを使ってマッチングの動作確認

--- text/AppendixB/example-B-06.php
+++ text/AppendixB/example-B-06.php
@@ -1,15 +1,19 @@
+<?php
+
 $ok_html  = "I <b>love</b> shrimp dumplings.";
 $bad_html = "I <b>love</i> shrimp dumplings.";
 
 if (preg_match('@<[bi]>.*?</[bi]>@',$ok_html)) {
-    print "Good for you! (OK, No backreferences)\n";
+    print "Good for you! (OK, No backreferences)<br>\n";
 }
 if (preg_match('@<[bi]>.*?</[bi]>@',$bad_html)) {
-    print "Good for you! (Bad, No backreferences)\n";
+    print "Good for you! (Bad, No backreferences)<br>\n";
 }
 if (preg_match('@<([bi])>.*?</\\1>@',$ok_html)) {
-    print "Good for you! (OK, Backreferences)\n";
+    print "Good for you! (OK, Backreferences)<br>\n";
 }
 if (preg_match('@<([bi])>.*?</\\1>@',$bad_html)) {
-    print "Good for you! (Bad, Backreferences)\n";
+    print "Good for you! (Bad, Backreferences)<br>\n";
+}else{
+	echo 'NG!!!';
 }
