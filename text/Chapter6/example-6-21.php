5fe9acc9d6810cf84649ad0bf341c1c9d6171789
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-10 20:20:03 +0900

    6.3.6 HTMLとJavaScriptの動作確認

--- text/Chapter6/example-6-21.php
+++ text/Chapter6/example-6-21.php
@@ -1,4 +1,14 @@
+<?php
+
+$_POST['comments'] = <<<_COMM_
+
+これはコメント<b>強調</b>です。<br>
+これはコメント<i>イタリック</i>です。
+
+_COMM_;
+
+
 // Remove HTML from comments
 $comments = strip_tags($_POST['comments']);
 // Now it's OK to print $comments
-print $comments.
+print $comments;
--- text/Chapter6/example-6-22.php
+++ text/Chapter6/example-6-22.php
@@ -1,3 +1,12 @@
+<?php
+
+$_POST['comments'] = <<<_COMM_
+
+これはコメント<b>強調</b>です。<br>
+これはコメント<i>イタリック</i>です。
+
+_COMM_;
+
 $comments = htmlentities($_POST['comments']);
 // Now it's OK to print $comments
-print $comments.
+print $comments;
