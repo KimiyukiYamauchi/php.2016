6dfcd63e2752b3701df7bb8e8f5e159ea9b6f57c
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-10 19:56:43 +0900

    例6-19 異なる選択と値つき<select>メニューの動作確認

--- text/Chapter6/example-6-19.php
+++ text/Chapter6/example-6-19.php
@@ -1,10 +1,43 @@
+<?php
+
+
 $sweets = array('puff' => 'Sesame Seed Puff',
                 'square' => 'Coconut Milk Gelatin Square',
                 'cake' => 'Brown Sugar Cake',
                 'ricemeat' => 'Sweet Rice and Meat');
 
+// $_POST配列に _submit_checkキーがあれば
+// サブミットされている
+if (array_key_exists('_submit_check', $_POST)) {
+    // validate_formから返されるエラーメッセージの配列が
+    // 空でなければ、エラメッセージ引数として、フォーム表示
+    if ($form_errors = validate_form()) {
+        show_form($form_errors);
+    } else {
+        // エラーメッセージの配列が空の場合は
+        // 処理を実行
+        process_form();
+    }
+} else {
+    // サブミットされていない場合はフォームを表示
+    show_form();
+}
+
+// サブミットされた値を処理する
+function process_form() {
+    echo $GLOBALS['sweets'][$_POST['order']] . 'が選択されました';
+}
+
 // Display the form
-function show_form() {
+function show_form($errors = '') {
+
+	// 引数としてエラーメッセージがあればそれを表示
+    if ($errors) {
+        print 'Please correct these errors: <ul><li>';
+        print implode('</li><li>', $errors);
+        print '</li></ul>';
+    }
+
     print<<<_HTML_
 <form method="post" action="$_SERVER[PHP_SELF]">
 Your Order: <select name="order">
@@ -21,4 +54,19 @@ print<<<_HTML_
 <input type="hidden" name="_submit_check" value="1">
 </form>
 _HTML_;
+}
+
+// サブミットされた値のチェック
+function validate_form() {
+    // エラーメッセージを格納する配列を初期化
+    $errors = array();
+
+    if(!array_key_exists($_POST['order'], $GLOBALS['sweets'])){
+    	$errors[] = '注文を正しく選択してください。';
+    }
+
+
+    // エラーメッセージが格納された配列を返す
+    // エラーがなければ配列の要素は空
+    return $errors;
 }
