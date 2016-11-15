ef79b897512f09dc67fbb1d535bef082fdc18f5e
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-15 22:01:58 +0900

    phpからdbへの接続確認

--- text/Chapter7/example-7-04.php
+++ text/Chapter7/example-7-04.php
@@ -1,3 +1,23 @@
-require 'DB.php';
-$db = DB::connect('mysql://penguin:top^hat@db.example.com/restaurant');
-if (DB::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+<?php
+
+require 'MDB2.php';	// PEARのMDB2モジュールをロード
+
+// db_program://ユーザ名:パスワード@ドメイン名/データベース名
+$db = MDB2::connect('mysqli://yamauchi:p@localhost/rensyu');
+if (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+
+
+$q = $db->query("CREATE TABLE dishes (
+	dish_id INT primary key autoincrement,
+	dish_name VARCHAR(255),
+	price DECIMAL(4,2),
+	is_spicy INT
+)");
+
+if (MDB2::isError($db)) { die("query error: " . $db->getMessage()); }
+
+
+//$q = $db->query("drop table dishes");
+
+
+echo 'OK!!!';
