9b63fbc472e64cccfa2ea68ca63e3a163a1de178
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-17 21:08:24 +0900

    例7-22 UPDATEまたはDELETEが影響する行を返す処理の動作確認

--- text/Chapter7/example-7-22.php
+++ text/Chapter7/example-7-22.php
@@ -1,6 +1,14 @@
-require 'DB.php';
-$db = DB::connect('mysql://hunter:w)mp3s@db.example.com/restaurant');
-if (DB::isError(db)) { die("connection error: " . $db->getMessage()); }
+<?php
+
+require 'MDB2.php';	// PEARのMDB2モジュールをロード
+
+// db_program://ユーザ名:パスワード@ドメイン名/データベース名
+$db = MDB2::connect('mysqli://yamauchi:p@localhost/rensyu');
+if (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+
+// この後のデータベースエラーに関してはメッセージを出力して抜け出す
+$db->setErrorHandling(PEAR_ERROR_DIE);
+
 // Decrease the price some some dishes
-$db->query("UPDATE dishes SET price=price - 5 WHERE price > 20");
-print 'Changed the price of ' . $db->affectedRows() . 'rows.';
+$count = $db->exec("UPDATE dishes SET price=price - 1 WHERE price > 5");
+print 'Changed the price of ' . $count . 'rows.';
