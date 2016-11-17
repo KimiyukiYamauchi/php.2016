5bdc98dc698b49bd4b8b48cda5387a3457b03d1f
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-17 21:09:41 +0900

    例7-27,7-28 プレースホルダによるデータの追加の動作確認

--- text/Chapter7/example-7-27.php
+++ text/Chapter7/example-7-27.php
@@ -1,2 +1,15 @@
-$db->query('INSERT INTO dishes (dish_name) VALUES (?)',
-    array($_POST['new_dish_name']));
+<?php
+
+require 'MDB2.php';	// PEARのMDB2モジュールをロード
+
+// db_program://ユーザ名:パスワード@ドメイン名/データベース名
+$db = MDB2::connect('mysqli://yamauchi:p@localhost/rensyu?charset=utf8');
+if (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+
+// この後のデータベースエラーに関してはメッセージを出力して抜け出す
+$db->setErrorHandling(PEAR_ERROR_DIE);
+
+$_POST['new_dish_name'] = 'オムライス';
+
+$sth = $db->prepare('INSERT INTO dishes (dish_name) VALUES (?)');
+$sth->execute(array($_POST['new_dish_name']));
--- text/Chapter7/example-7-28.php
+++ text/Chapter7/example-7-28.php
@@ -1,3 +1,19 @@
-$db->query('INSERT INTO dishes (dish_name,price,is_spicy) VALUES (?,?,?)',
-           array($_POST['new_dish_name'], $_POST['new_price'],
-                 $_POST['is_spicy']));
+<?php
+
+require 'MDB2.php';	// PEARのMDB2モジュールをロード
+
+// db_program://ユーザ名:パスワード@ドメイン名/データベース名
+$db = MDB2::connect('mysqli://yamauchi:p@localhost/rensyu?charset=utf8');
+if (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+
+// この後のデータベースエラーに関してはメッセージを出力して抜け出す
+$db->setErrorHandling(PEAR_ERROR_DIE);
+
+$_POST['new_dish_name'] = 'カツカレー';
+$_POST['new_price'] = 7;
+$_POST['is_spicy'] = 1;
+
+$sth = $db->prepare('INSERT INTO dishes (dish_name,price,is_spicy) VALUES (?,?,?)');
+$sth->execute(array($_POST['new_dish_name'], $_POST['new_price'], $_POST['is_spicy']));
+
+echo 'OK!!!';
