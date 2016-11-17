4be5f3462af41d1406d115e2b4fe3ab80a4c8b20
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-17 21:10:21 +0900

    例7-29 シーケンスからIDを得る処理の動作確認

--- text/Chapter7/example-7-29.php
+++ text/Chapter7/example-7-29.php
@@ -1,3 +1,19 @@
-$dish_id = $db->nextID('dishes');
-$db->query("INSERT INTO orders (dish_id, dish_name, price, is_spicy)
+<?php
+
+require 'MDB2.php';	// PEARのMDB2モジュールをロード
+
+// db_program://ユーザ名:パスワード@ドメイン名/データベース名
+$db = MDB2::connect('mysqli://yamauchi:p@localhost/rensyu?charset=utf8');
+if (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+
+// この後のデータベースエラーに関してはメッセージを出力して抜け出す
+$db->setErrorHandling(PEAR_ERROR_DIE);
+
+$dish_id = $db->nextID('dishes2');
+
+echo '$dish_id = ' . $dish_id . '<br>';
+
+$db->query("INSERT INTO dishes2 (dish_id, dish_name, price, is_spicy)
     VALUES ($dish_id, 'Fried Bean Curd', 1.50, 0)");
+
+echo 'OK!';
