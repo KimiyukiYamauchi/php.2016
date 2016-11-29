989df252a096668ad96c39b40231f4f158faf3b8
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-11-29 17:38:58 +0900

    7.9 取り出した行の書式を変更するの動作確認

--- text/Chapter7/example-7-47.php
+++ text/Chapter7/example-7-47.php
@@ -1,25 +1,33 @@
-require 'DB.php';
-$db = DB::connect('mysql://hunter:w)mp3s@db.example.com/restaurant');
+<?php
 
-// Change the fetch mode to objects
-$db->setFetchMode(DB_FETCHMODE_OBJECT);
+require 'MDB2.php';	// PEARのMDB2モジュールをロード
 
-print "With query() and fetchRow(): \n";
+// db_program://ユーザ名:パスワード@ドメイン名/データベース名
+$db = MDB2::connect('mysqli://yamauchi:p@localhost/rensyu?charset=utf8');
+if (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); }
+
+// この後のデータベースエラーに関してはメッセージを出力して抜け出す
+$db->setErrorHandling(PEAR_ERROR_DIE);
+
+// フェッチモードをオブジェクトに変更
+$db->setFetchMode(MDB2_FETCHMODE_OBJECT);
+
+print "With query() and fetchRow(): <br>\n";
 // get each row with query() and fetchRow();
 $q = $db->query("SELECT dish_name, price FROM dishes");
 while($row = $q->fetchRow()) {
-    print "The price of $row->dish_name is $row->price \n";
+    print "The price of $row->dish_name is $row->price <br>\n";
 }
 
 
-print "With getAll(): \n";
+print "With getAll(): <br>\n";
 // get all the rows with getAll();
-$dishes = $db->getAll('SELECT dish_name, price FROM dishes');
+$dishes = $db->queryAll('SELECT dish_name, price FROM dishes');
 foreach ($dishes as $dish) {
-    print "The price of $dish->dish_name is $dish->price \n";
+    print "The price of $dish->dish_name is $dish->price <br>\n";
 }
 
-print "With getRow(): \n";
-$cheap = $db->getRow('SELECT dish_name, price FROM dishes
+print "With getRow(): <br>\n";
+$cheap = $db->queryRow('SELECT dish_name, price FROM dishes
     ORDER BY price LIMIT 1');
 print "The cheapest dish is $cheap->dish_name with price $cheap->price";
