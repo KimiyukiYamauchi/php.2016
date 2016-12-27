a0d03cda53e7f4f26b3595538ccf4588dde8270c
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-12-27 19:04:52 +0900

    7章の動作確認のための修正

--- text/Chapter7/example-7-56-pdo-error.php
+++ text/Chapter7/example-7-56-pdo-error.php
@@ -1,29 +1,34 @@
 <?php
-
+echo 'MDB2とPDOのプログラムの記述の比較';
+// PEARのMDB2モジュールをロード
+// require 'MDB2.php';
 // フォームヘルパー関数をロード
 require '../Chapter6/formhelpers.php';
 
+// Connect to the database
+//$db = MDB2::connect('mysqli://yamashiro:pass@localhost/rensyu?charset=utf8');
+//f (MDB2::isError($db)) { die("Can't connect: " . $db->getMessage()); //}
 try{
-    $dns = 'mysql:host=localhost;dbname=rensyu;charset=utf8';
+    $dns = 'mysql:localhost;dbname=rensyu;charset=utf8';
     $username = 'yamauchi';
     $password = 'p';
-    $db = new PDO($dns, $username, $password);
-
-    // エラーになったら例外を投げる
+    $db = new PDO($dns,$username,$password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-}catch(PDOexception $e){
+} catch(PDOexception $e) {
     var_dump($e);
 }
 
-//var_dump($db);
+// 自動でエラーハンドリングを設定
+//$db->setErrorHandling(PEAR_ERROR_DIE);
 
-// フォームのメニューでの"spicy"の選択肢
+// フェッチモードを設定：行をオブジェクトとする
+//$db->setFetchMode(MDB2_FETCHMODE_OBJECT);
+
+// フォームのメニューでの spicy の選択肢
 $spicy_choices = array('no','yes','either');
 
-// メインページのロジック:
-// - フォームがサブミットされたら、
-//    検証し、処理またはエラー付きフォーム表示
+// メインページのロジック
+// - フォームがサブミットされたら、検証し、処理またはエラー付きフォーム表示
 // - フォームがサブミットされていない場合はフォームを表示
 if (array_key_exists('_submit_check', $_POST)) {
     // If validate_form() returns errors, pass them to show_form()
@@ -39,26 +44,26 @@ if (array_key_exists('_submit_check', $_POST)) {
 }
 
 function show_form($errors = '') {
-    // フォームがサブミットされたら、サブミットされた
-    // パラメータからデフォルトを取り出す
+    // フォームがサブミットされたら、
+    // サブミットされたパラメータからデフォルトを取り出す
     if (array_key_exists('_submit_check', $_POST)) {
         $defaults = $_POST;
     } else {
         // サブミットされていなければ、独自のデフォルトをセット
         $defaults = array('min_price' => '5.00',
                           'max_price' => '25.00',
-                          'dish_name' => '',
+                          'dish_name' => ''
                           );
     }
-    
-    // エラーが渡されると、$error_textに代入(HTMLマークアップ形式)
+
+    // エラーが渡されると、 $error_text に代入
     if (is_array($errors)) {
         $error_text = '<tr><td>You need to correct the following errors:';
         $error_text .= '</td><td><ul><li>';
         $error_text .= implode('</li><li>',$errors);
         $error_text .= '</li></ul></td></tr>';
     } else {
-        // エラーがなければ、$error_textは空を設定
+        // エラーがない場合は、何も出力しない
         $error_text = '';
     }
 
@@ -87,23 +92,25 @@ function show_form($errors = '') {
 </table>
 <input type="hidden" name="_submit_check" value="1"/>
 </form>
+
+
 <?php
-      } // show_form()の終わり
+} // The end of show_form()
 
 function validate_form() {
     $errors = array();
 
-    // 最低価格は妥当な浮動小数点数でなくてはならない
+    // 最低価格は妥当な浮動小数点かどうかのチェック
     if ($_POST['min_price'] != strval(floatval($_POST['min_price']))) {
         $errors[] = 'Please enter a valid minimum price.';
     }
 
-    // 最高価格は妥当な浮動小数点数でなくてはならない
+    // 最高価格は妥当な浮動小数点かどうかのチェック
     if ($_POST['max_price'] != strval(floatval($_POST['max_price']))) {
         $errors[] = 'Please enter a valid maximum price.';
     }
 
-    // 最低価格は最高価格より低くなくてはならない
+    // 最高価格と最低価格の妥当性をチェック
     if ($_POST['min_price'] >= $_POST['max_price']) {
         $errors[] = 'The minimum price must be less than the maximum price.';
     }
@@ -115,31 +122,31 @@ function validate_form() {
 }
 
 function process_form() {
-    // この関数内で、グローバル変数の$dbにアクセスする
+    // この関数内で、グローバル変数の$dbを読み込む
     global $db;
 
-    try{
-    
-        // 問い合わせのSQL文を作成
-        $sql = 'SELECT dish_name, price, is_spicy FROM dishes WHERE
-                price >= ? AND price <= ?';
+    try {
+        // 問い合わせのSQL文を記述
+        //$sql = 'SELECT dish_name, price, is_spicy FROM dishes WHERE
+        //        price >= :min_price AND price <= :max_price';
+         $sql = 'SELECT dish_name, price, is_spicy FROM dishes WHERE
+                price >= 1 AND price <= 25';
 
         // 料理名が入っていれば、where句に追加
-        // ユーザが入力したSQLのワイルドカードを避けるために
-        // quote()とstrtr()を追加
+        // ユーザーが入力したSQLのワイルドカードを避ける quote()とstrtr() を追加
         if (strlen(trim($_POST['dish_name']))) {
             $dish = $db->quote($_POST['dish_name']);
             $dish = strtr($dish, array('_' => '\_', '%' => '\%'));
 
-            // 部分一致で検索できるように検索文字列の前後に%を追加
+            // 部分一致で検索できるように検索文字列の前後に％を追加
             $dish = preg_replace('/^\'/', '\'%', $dish);
             $dish = preg_replace('/\'$/', '%\'', $dish);
 
             $sql .= " AND dish_name LIKE $dish";
         }
 
-        // is_spicyが"yes" または "no"の時はSQLを追加
-        // "either"なら何もしない
+        // if is_spicy が "yes" or "no" の時はSQLを追加
+        // either なら何もしない
         $spicy_choice = $GLOBALS['spicy_choices'][ $_POST['is_spicy'] ];
         if ($spicy_choice == 'yes') {
             $sql .= ' AND is_spicy = 1';
@@ -147,14 +154,29 @@ function process_form() {
             $sql .= ' AND is_spicy = 0';
         }
 
-        // プリペアードクエリ
+        // クエリをDBプログラムに送り、戻ってくるすべての行を取得
+
         $sth = $db->prepare($sql);
-        $sth->execute(array($_POST['min_price'],$_POST['max_price']));
-        // フェッチモードをオブジェクトに設定
+
+        //var_dump($sth);
+
+        //$result = $sth->execute(array($_POST['min_price'], $_POST['max_price']));
+        //$dishes = $result->fetchAll();
+        //$ret = $sth->execute(array($_POST['min_price'],$_POST['max_price']));
+        //$ret = $sth->execute(array(':min_price' => $_POST['min_price'],
+         //                           ':max_price' => $_POST['max_price']));
+        $ret = $sth->execute(array());
+        //var_dump($ret);
+
+        // フェッチモードの設定
         $sth->setFetchMode(PDO::FETCH_OBJ);
         // 結果の取得
         $dishes = $sth->fetchAll();
-        
+
+
+        var_dump($sql);
+        var_dump($dishes);
+
         if (count($dishes) == 0) {
             print 'No dishes matched.';
         } else {
@@ -170,7 +192,7 @@ function process_form() {
                        htmlentities($dish->dish_name), $dish->price, $spicy);
             }
         }
-    }catch(PDOexception $e){
+    } catch (PDOexception $e) {
         var_dump($e);
     }
 }
